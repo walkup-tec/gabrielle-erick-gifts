008d69864a8c0f5bd400de92aff80cd3c0f35d0b
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-11 12:43:36 -0300

    Adiciona o campo unit na tabela de presentes

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -20,6 +20,7 @@ export type Database = {
           desired_quantity: number
           id: string
           name: string
+          unit: string
           updated_at: string
         }
         Insert: {
@@ -27,6 +28,7 @@ export type Database = {
           desired_quantity: number
           id?: string
           name: string
+          unit?: string
           updated_at?: string
         }
         Update: {
@@ -34,6 +36,7 @@ export type Database = {
           desired_quantity?: number
           id?: string
           name?: string
+          unit?: string
           updated_at?: string
         }
         Relationships: []
